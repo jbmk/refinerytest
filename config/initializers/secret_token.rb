e129e1628748f13bed928d51e7fa3cc70911b8b6
Bala Murali Krishna <jbmk@milaap.org>
2012-02-15 17:48:29 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinerytest::Application.config.secret_token = '17fa7a19781ff357b84fd1dada4020538d17a3857841100501581d300d5912764894a8506e50fffa48c6d461cd85801d8ea874371ff12aa17a9d9e7bee0138c6'
