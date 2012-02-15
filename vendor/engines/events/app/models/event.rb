1be8f63464966a8ab5360ba33c68c239ed8b49e2
Bala Murali Krishna <jbmk@milaap.org>
2012-02-15 18:41:58 +0530

    first commit

---
+++ vendor/engines/events/app/models/event.rb
@@ -0,0 +1,8 @@
+class Event < ActiveRecord::Base
+
+  acts_as_indexed :fields => [:title, :description]
+
+  validates :title, :presence => true, :uniqueness => true
+  
+  belongs_to :photo, :class_name => 'Image'
+end
